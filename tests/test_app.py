ccb65e92a3c656cc2c33e1923a4495442b3b8c83
Alexandr Prohnitskii <alexandprohnitskii@gmail.com>
2026-08-16 01:26:10 +0400

    fix: update tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,4 +7,6 @@ def test_health():
     response = client.get("/health")
 
     assert response.status_code == 200
-    assert response.json == {"status": "ok"}
+    assert response.json == {"status": "ok",
+          		     "service": "flask_project"
+			    }
