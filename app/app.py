19454f88159d961f286f46b028d51edf3f7eedab
Alexandr Prohnitskii <alexandprohnitskii@gmail.com>
2026-08-16 02:00:12 +0400

    fix: update app.py

--- app/app.py
+++ app/app.py
@@ -17,5 +17,8 @@ def info():
             "version": "1.0"
             }
 
+@app.route("/version")
+def version():
+    return { "version": "1.0" }
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
